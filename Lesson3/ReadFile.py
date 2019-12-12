de0d8b6585441d8d8c641e4e8bc49e9b9e6c489c
DashBez <dashbez95@gmail.com>
2019-12-12 16:16:30 +0300

    test

--- Lesson3/ReadFile.py
+++ Lesson3/ReadFile.py
@@ -7,10 +7,10 @@ with open('referat.txt','r',encoding='utf-8') as f:
         if letter == ' ' or letter == '\n':
             count_words += 1 
         
-        if letter == '.':
-            content = content.replace('.','!')
+        content = content.replace('.','!')
     print(count_words-1)
     #print(content)
     file_for_write = open('referat2.txt','w')
     file_for_write.write(content)
-    file_for_write.close()
+    file_for_write.close() #git test
+
