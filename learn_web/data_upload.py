201716c5d3a412d37e24c99f8868b874589002a3
DashBez <dashbez95@gmail.com>
2020-01-11 00:28:20 +0300

    add_price_image_seller

--- learn_web/data_upload.py
+++ learn_web/data_upload.py
@@ -44,13 +44,19 @@ def get_info_goods(url,name_block,key_dict,value_dict):
             part_of_Size_tmp =  soup.find(name_block, { key_dict: value_dict}).find('p',class_= 'col-7 col-sm-12').text
             part_of_Condition_tmp = soup.find(name_block, { key_dict: value_dict}).find('div',class_='col-7 col-sm-12').find('p').text
             part_of_Description_tmp = soup.find(name_block, { key_dict: value_dict}).find("ul", {"id":"product-description"})
+            part_of_price_tmp = soup.find(name_block, { key_dict: value_dict}).find('div',class_='d-flex flex-column align-self-center text-center product-detail__price mr-sm-3').text
+            part_of_seller_tmp = soup.find(name_block, { key_dict: value_dict}).find('div',class_= 'product__seller card card-body mb-3')
+            part_of_image_tmp = soup.find(name_block, { key_dict: value_dict}).find('div',class_= 'card d-none d-lg-block gap')
 
             result_info.append ({
                 'Marka': " ".join(part_of_Marka_tmp.split()),
                 'Size': " ".join(part_of_Size_tmp.split()),
                 'Condition': " ".join(part_of_Condition_tmp.split()),
-                'Description': part_of_Description_tmp
-               # 'test': re.findall(part_of_Description_tmp))
+                'Description': part_of_Description_tmp,
+                'Price': " ".join(part_of_price_tmp.split()),
+                'Seller': part_of_seller_tmp,
+                'Image': part_of_image_tmp
+                #'whole part': soup.find(name_block, { key_dict: value_dict})
  
                 })
         except:
@@ -66,9 +72,10 @@ if __name__ == "__main__":
     post = []
     Goods_info = get_href_goods("https://www.maedchenflohmarkt.de/alle-ansehen.html",'a','class','product-image orientation-portrait')
     for Goods in Goods_info:
-        href_info = get_info_goods("https://www.maedchenflohmarkt.de"+Goods['href'],'div','class','product__attributes card card-body gap')
+        #href_info = get_info_goods("https://www.maedchenflohmarkt.de"+Goods['href'],'div','class','product__attributes card card-body gap')
+        href_info = get_info_goods("https://www.maedchenflohmarkt.de"+Goods['href'],'div','id','product-detail')
         for info in href_info:
-            with open("href_info.txt",'a',encoding='utf8') as f:
+            with open("href_info.txt",'w',encoding='utf8') as f:
                 f.write(str(info))
 
 
