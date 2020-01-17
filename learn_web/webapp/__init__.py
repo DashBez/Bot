5096788682d99a4f5b4cfc69aca8a72d7e8f7547
DashBez <dashbez95@gmail.com>
2020-01-17 15:24:17 +0300

    start_add_login

--- learn_web/webapp/__init__.py
+++ learn_web/webapp/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template
+from webapp.forms import  LoginForm
 from webapp.model import db,News
 from webapp.weather  import weather_by_city
 #from webapp.python_org_news import get_python_news
@@ -14,11 +15,16 @@ def create_app():
     def index():
         title = 'Новости Python'
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
-        news_list = News.query.order_by(News.published.desc()).all()
-            
+        news_list = News.query.order_by(News.published.desc()).all()     
 
         return render_template('index.html',page_title=title,weather=weather,news_list=news_list)
-
+ 
+    @app.route('/login')
+    def login():
+        title = 'Авторизация'
+        login_form = LoginForm()
+        return render_template('login.html',page_title = title,form = login_form)
+    
     return  app
 
 
--- learn_web/webapp/config.py
+++ learn_web/webapp/config.py
@@ -7,3 +7,5 @@ WEATHER_DEFAULT_CITY = 'Saint-Petersburg'
 WEATHER_API_KEY = "209eedf3a15248dab5d40221191012"
 WEATHER_URL = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
+
+SECRET_KEY =  "209eedf3a15248dab5d40221191012"
