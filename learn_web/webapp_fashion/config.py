8f1336b72870302f4fd045eb034db3310993a703
DashBez <dashbez95@gmail.com>
2020-01-29 16:13:08 +0300

    Добавлена авторизация пользователей

--- learn_web/webapp_fashion/config.py
+++ learn_web/webapp_fashion/config.py
@@ -2,3 +2,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__)) 
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(basedir, '..', 'webapp_fashion.db')
+
+SECRET_KEY = "aasdfghjkl45678909jhgcvbnm"
